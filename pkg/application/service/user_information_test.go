8ada806cc412b584dc41c13f5803fe3a05162b90
neverett8fr <neverett8fr@btinternet.com>
2022-10-27 01:10:42 +0100

    added in unit tests / integration - must fix at some point

---
+++ pkg/application/service/user_information_test.go
@@ -0,0 +1,126 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreateNewUserIsSuccessful(t *testing.T) {
+
+	dbMock, mock := newMockDB(t)
+	mock = newMockAddUser(t, mock)
+	defer dbMock.Close()
+
+	router := newMockRouter(t, dbMock)
+
+	newRequest := newUserIn{
+		Username: realTestValueUsername,
+		Password: realTestValuePass,
+	}
+
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(newRequest)
+
+	path := fmt.Sprintf("/user")
+	req, err := http.NewRequest(http.MethodPost, path, &buf)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("error, expected %v, got %v", http.StatusOK, rr.Code)
+	}
+}
+
+func TestCreateNewUserInvalidUsernameIsNotSuccessful(t *testing.T) {
+
+	dbMock, mock := newMockDB(t)
+	mock = newMockAddUser(t, mock)
+	defer dbMock.Close()
+
+	router := newMockRouter(t, dbMock)
+
+	newRequest := newUserIn{
+		Username: "",
+		Password: realTestValuePass,
+	}
+
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(newRequest)
+
+	path := fmt.Sprintf("/user")
+	req, err := http.NewRequest(http.MethodPost, path, &buf)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code == http.StatusOK {
+		t.Errorf("error, expected %v, got %v", http.StatusOK, rr.Code)
+	}
+}
+
+func TestCreateNewUserInvalidPasswordIsNotSuccessful(t *testing.T) {
+
+	dbMock, mock := newMockDB(t)
+	mock = newMockAddUser(t, mock)
+	defer dbMock.Close()
+
+	router := newMockRouter(t, dbMock)
+
+	newRequest := newUserIn{
+		Username: realTestValueUsername,
+		Password: "",
+	}
+
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(newRequest)
+
+	path := fmt.Sprintf("/user")
+	req, err := http.NewRequest(http.MethodPost, path, &buf)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code == http.StatusOK {
+		t.Errorf("error, expected %v, got %v", http.StatusOK, rr.Code)
+	}
+}
+func TestCreateNewUserNoBodyIsNotSuccessful(t *testing.T) {
+
+	dbMock, mock := newMockDB(t)
+	mock = newMockAddUser(t, mock)
+	defer dbMock.Close()
+
+	router := newMockRouter(t, dbMock)
+
+	newRequest := newUserIn{}
+
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(newRequest)
+
+	path := fmt.Sprintf("/user")
+	req, err := http.NewRequest(http.MethodPost, path, &buf)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Code == http.StatusOK {
+		t.Errorf("error, expected %v, got %v", http.StatusOK, rr.Code)
+	}
+}
---
+++ pkg/application/service/utils_test.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"tempest-user-service/pkg/infra/db"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/gorilla/mux"
+)
+
+const (
+	realTestValueUsername   = "username1"
+	realTestValuePass       = "123Password"
+	realTestValueHashedPass = "213PretendHashed"
+
+	userColumnID           = "id"
+	userColumnUsername     = "username"
+	userColumnPasswordHash = "password_hash"
+
+	userTableUsers = "users"
+)
+
+func newMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	dbMock, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return dbMock, mock
+}
+
+type anyPass struct{}
+
+func (a anyPass) Match(v driver.Value) bool {
+	_, ok := v.(string)
+	return ok
+}
+
+func newMockAddUser(t *testing.T, m sqlmock.Sqlmock) sqlmock.Sqlmock {
+
+	mockQueryGetUserRows := sqlmock.NewRows(
+		[]string{"id"},
+	)
+	mockExecNewUserRows := sqlmock.NewResult(0, 1)
+
+	// regexp.QuoteMeta()
+	m.ExpectQuery(fmt.Sprintf("SELECT %s FROM %s WHERE %s = $1", userColumnID, userTableUsers, userColumnUsername)).WithArgs(
+		realTestValueUsername,
+	).WillReturnRows(mockQueryGetUserRows)
+	m.ExpectExec(fmt.Sprintf("INSERT INTO %s(%s, %s) VALUES($1, $2)", userTableUsers, userColumnUsername, userColumnPasswordHash)).
+		WithArgs(
+			realTestValueUsername,
+			anyPass{},
+		).WillReturnResult(mockExecNewUserRows)
+
+	return m
+}
+
+func newMockRouter(t *testing.T, dbMock *sql.DB) *mux.Router {
+
+	mockDBConn := db.NewDBConnFromExisting(dbMock)
+
+	router := mux.NewRouter()
+	NewUserInformation(router, mockDBConn.Conn)
+
+	return router
+}
